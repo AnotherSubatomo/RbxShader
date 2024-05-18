a3ad760467a7ac5c8467678eb261450595557671
anosuba <112761665+AnotherSubatomo@users.noreply.github.com>
2024-05-18 13:15:40 +0800

    Update Worker.lua

--- src/Worker.lua
+++ src/Worker.lua
@@ -11,14 +11,16 @@
 
 --!native
 
+export type ShaderBuffer = (
+	fragColor : Vector3 ,
+	fragCoords : Vector2 ,
+	iTime : number ,
+	iTimeDelta : number ,
+	iResolution : Vector2	
+) -> (number, number, number, number)
+
 export type Shader = {
-	mainImage : (
-		fragColor : Vector3 ,
-		fragCoords : Vector2 ,
-		iTime : number ,
-		iTimeDelta : number ,
-		iResolution : Vector2	
-	) -> (number, number, number, number)
+	mainImage : ShaderBuffer
 }
 
 -- // Dependencies
@@ -30,6 +32,15 @@ local RbxShader = game.ReplicatedStorage:FindFirstChild('RbxShader', true)
 -- ^^^ Replace this with `script.Parent` during debugging for type-checking...
 local CanvasDraw = require(RbxShader.CanvasDraw)
 local ImageDataConstructor = require(RbxShader.CanvasDraw:WaitForChild("ImageDataConstructor"))
+
+local STRegistry = game:GetService('SharedTableRegistry')
+
+local ERROR = {
+	'SHADER@EUN: Personal canvas does not exist.' ,
+	'SHADER@RUN: Shader never had a `mainImage` function.' ,
+	'SHADER@RUN: Buffers should not share the same letter.' ,
+	'SHADER@RUN: Buffer amount should not exceed 26. (A-Z)'
+}
 
 local RenderConnection : RBXScriptConnection = nil
 local CanvasCoords : Vector2 = nil
@@ -59,89 +70,132 @@ end)
 
 local iResolution : Vector2 = nil
 local Shader : Shader = nil
+local ShaderID : string = nil
 local InterlaceFactor : number = nil
 local DualAxisInterlacing : boolean = nil
 
+local function SetConfigurations( Configuration : EngineConfiguration )
+	InterlaceFactor = Configuration.InterlaceFactor
+	DualAxisInterlacing = Configuration.DualAxisInterlacing
+end
 
 
-Actor:BindToMessage( 'Set' , function (
+
+Actor:BindToMessage( 'Initialize' , function (
 	__iResolution : Vector2 ,
 	__Shader : ModuleScript ,
-	__InterlaceFactor : number ,
-	__DualAxisInterlacing : boolean
+	__ShaderID : string ,
+	Configuration : EngineConfiguration
 )
-	assert( Canvas , 'SHADER@PARALLEL: Personal canvas does not exist.' )
+	assert( Canvas , ERROR[1] )
 	
 	iResolution = __iResolution
 	Shader = require(__Shader)
-	InterlaceFactor = __InterlaceFactor
-	DualAxisInterlacing = __DualAxisInterlacing
+	ShaderID = __ShaderID
+	SetConfigurations( Configuration )
 end)
 
 
 
-Actor:BindToMessageParallel( 'Run' , function ()
-	-- /* using os.clock() is smoother than os.time(), pretty sick */
-	local oTime = os.clock()
-	local function iTime()
-		return os.clock() - oTime
+Actor:BindToMessage( 'Set' , function (
+	Configuration : EngineConfiguration
+)
+	SetConfigurations( Configuration )
+end)
+
+
+
+-- /* using os.clock() is smoother than os.time(), pretty sick */
+local oTime = os.clock()
+local pTime = 0
+
+-- /* Program runtime getter */
+local function iTime()
+	return os.clock() - oTime
+end
+
+-- /* Program runner */
+local function RunProgram()
+	
+	-- /* Buffer implementation */
+	local Buffers = {}
+	for Step : string , func : ShaderBuffer in Shader do
+		if Step:sub(1, 6) ~= 'buffer' then continue end
+		local Order = Step:sub(7, 7):upper():byte() - 64
+		assert( Buffers[Order] , ERROR[3] )
+		Buffers[Order] = func
 	end
 
-	-- /* Buffer-accumulation implementation */
-	--	  Canvases are actually already their own
-	--	  buffer, pretty neat huh! Thanks Ethan! <3
+	-- # Buffer sequence re-ordering
+	local __ = {}
+	for __ , func in Buffers do
+		table.insert(__, func)
+	end
+	Buffers = __
+
+	assert( Shader.mainImage , ERROR[2] )
+	table.insert(Buffers, Shader.mainImage)
+	
+	-- /* Fetch inputs */
+	local iMouse = STRegistry:GetSharedTable(ShaderID..'@Mouse')
 
 	-- /* Interlacer implementation */
 	local Step = 1
 	local StepX, StepY = 1, 1
-	
+
 	local Offset = Canvas.Resolution * (CanvasCoords - Vector2.one)
 
 	-- /* Render functions */
 	-- @ with dual-axis interlacing
 	local function DUIRendering ( iTimeDelta : number )
-		
+
 		if StepX > InterlaceFactor then StepX = 1; StepY += 1 end
 		if StepY > InterlaceFactor then StepY = 1 end
 		local iTime = iTime()
 
-		for y = StepY , Canvas.Resolution.Y, InterlaceFactor do
-			for x = StepX, Canvas.Resolution.X, InterlaceFactor do
-				Canvas:SetRGBA(x, y,
-					Shader.mainImage(
-						Vector3.new(Canvas:GetRGB(x, y)),
-						Vector2.new(x + Offset.X, y + Offset.Y),
-						iTime,
-						iTimeDelta,
-						iResolution
+		for __ , Buffer : ShaderBuffer in Buffers do
+			for y = StepY , Canvas.Resolution.Y, InterlaceFactor do
+				for x = StepX, Canvas.Resolution.X, InterlaceFactor do
+					Canvas:SetRGBA(x, y,
+						Buffer(
+							Vector3.new(Canvas:GetRGB(x, y)),
+							Vector2.new(x + Offset.X, y + Offset.Y),
+							iTime,
+							iTimeDelta,
+							iResolution,
+							iMouse
+						)
 					)
-				)
+				end
 			end
 		end
-		
+
 		task.synchronize()
-		
+
 		Canvas:Render()
 		StepX += 1
 	end
-	
+
 	-- @ without dual-axis interlacing
 	local function UUIRendering ( iTimeDelta : number )
 
 		if Step > InterlaceFactor then Step = 1 end
 		local iTime = iTime()
 
-		for y = Step , Canvas.Resolution.Y, InterlaceFactor do
-			for x = 1, Canvas.Resolution.X do
-				Canvas:SetRGBA(x, y,
-					Shader.mainImage(
-						Vector3.new(Canvas:GetRGB(x, y)),
-						Vector2.new(x + Offset.X, y + Offset.Y),
-						iTime,
-						iTimeDelta,
-						iResolution
+		for __ , Buffer : ShaderBuffer in Buffers do
+			for y = Step , Canvas.Resolution.Y, InterlaceFactor do
+				for x = 1, Canvas.Resolution.X do
+					Canvas:SetRGBA(x, y,
+						Buffer(
+							Vector3.new(Canvas:GetRGB(x, y)),
+							Vector2.new(x + Offset.X, y + Offset.Y),
+							iTime,
+							iTimeDelta,
+							iResolution,
+							iMouse
+						)
 					)
-				)
+				end
 			end
 		end
 
@@ -150,17 +204,43 @@ Actor:BindToMessageParallel( 'Run' , function ()
 		Canvas:Render()
 		Step += 1
 	end
-	
+
 	-- /* Rendering */
 	local PerFrame = DualAxisInterlacing and DUIRendering or UUIRendering
 
 	task.synchronize()
 
 	RenderConnection = Run.PreRender:ConnectParallel( PerFrame )
+end
+
+
+
+Actor:BindToMessageParallel( 'Run' , function ()
+	-- /* Reset runtime. */
+	oTime = os.clock()
+	RunProgram()
+end)
+
+
+
+Actor:BindToMessage( 'Pause' , function()
+	pTime = iTime()
+	if RenderConnection ~= nil then
+		RenderConnection:Disconnect()
+	end
+end)
+
+
+
+Actor:BindToMessageParallel( 'Resume' , function()
+	oTime += iTime() - pTime
+	RunProgram()
 end)
 
 
 
 Actor:BindToMessage( 'Stop' , function ()
-	RenderConnection:Disconnect()
+	if RenderConnection ~= nil then
+		RenderConnection:Disconnect()
+	end
 end)
